00aa852b36c5a480bb418ed74cc7f20a9b260c5f
PC2 <gormeet@seznam.cz>
2022-09-07 14:59:16 +0200

    Ability to change thought color

--- src/scripts/add-thought.js
+++ src/scripts/add-thought.js
@@ -4,11 +4,26 @@ const addForm = document.querySelector('.new-thought-form');
 
 const input = document.querySelector('.thought-name');
 const thoughts_list = document.querySelector('.thoughts-list')
+const header = document.querySelector('.new-thought-header');
 let noItemSpan;
+let currentColor = "blue";
 
 const addBtn = document.querySelector('.add-new-thought');
 const submitBtn = document.querySelector('.btn-submit-thought');
 const removeBtn = document.querySelector('.btn-close-new');
+
+const blue = document.querySelector('.choice-blue');
+const red = document.querySelector('.choice-red');
+const green = document.querySelector('.choice-green');
+const orange = document.querySelector('.choice-orange');
+const choice = document.querySelectorAll('.color-choice');
+
+
+
+
+/*****************************VARIABLES*************************/
+
+
 
 addBtn.addEventListener('click', function (e) {
     addForm.style.opacity = 1;
@@ -17,6 +32,8 @@ addBtn.addEventListener('click', function (e) {
     console.log("Click");
     input.value = "";
     input.focus();
+    addForm.style.animationName = 'shadow-'+currentColor;
+
 })
 
 
@@ -25,6 +42,7 @@ removeBtn.addEventListener('click', function (e) {
     addForm.style.opacity = 0;
     addForm.style.visibility = 'hidden';
     addForm.classList.remove('shadow-anim');
+    addForm.style.animationName = null;
 })
 
 
@@ -33,7 +51,7 @@ input.addEventListener('keypress', function (e) {
     if (e.key === "Enter") {
         e.preventDefault();
         submitBtn.click();
-      }
+    }
 })
 
 
@@ -42,9 +60,11 @@ input.addEventListener('keypress', function (e) {
 submitBtn.addEventListener('click', function (e) {
     const newDiv = document.createElement("div");
     newDiv.className = "item"
+    newDiv.style.backgroundImage = 'var(--gradient-' + currentColor + ')';
+    
     const newSpan = document.createElement("span");
     var text = input.value;
-    if(text === ""){
+    if (text === "") {
         text = "Unnamed thought"
     }
     const newContent = document.createTextNode(text);
@@ -52,13 +72,22 @@ submitBtn.addEventListener('click', function (e) {
     newSpan.appendChild(newContent);
     newDiv.appendChild(newSpan);
 
-    
+
     thoughts_list.appendChild(newDiv);
-    
+
+
+    newDiv.addEventListener("mouseover", function(e) {
+        newDiv.style.boxShadow = '0 1px 5px var(--clr-shadow), 0 0 50px var(--clr-shadow-'+currentColor+')';
+    });
+    newDiv.addEventListener("mouseleave", function(e) {
+        newDiv.style.boxShadow = 'none';
+    });
+
+
     input.value = "";
     input.focus();
 
-    if(currentID != undefined){
+    if (currentID != undefined) {
         currentID = currentID + 1;
         console.log("xd")
     }
@@ -66,12 +95,86 @@ submitBtn.addEventListener('click', function (e) {
 
     let _item = {
         thought_name: text,
-        id: id_curr
+        id: id_curr,
+        color: currentColor
     };
     ipcRenderer.send("save_thought", _item);
-   
-    //HIDE NO-THOUGHTS SPAN
 
-    thoughts_list.removeChild(noItemSpan);
+    //HIDE NO-THOUGHTS SPAN
+    noItemSpan = document.querySelector('.no-item-span');
+    if(noItemSpan != null){
+        thoughts_list.removeChild(noItemSpan);
+    }
     
+
 })
+
+
+
+
+// COLOR CHANGE
+
+function changeColor(color) {
+    submitBtn.style.backgroundImage = 'var(--gradient-' + color + ')';
+    currentColor = color;
+    submitBtn.addEventListener("mouseover", function(){
+        submitBtn.style.boxShadow = '0 0 5px var(--clr-shadow), 0 0 20px var(--clr-shadow-'+color+')';
+
+    });
+    submitBtn.addEventListener("mouseleave", function(){
+        submitBtn.style.boxShadow = 'none';
+
+    });
+    header.style.backgroundImage = 'var(--gradient-' + color + ')';
+    addForm.style.animationName = 'shadow-'+color;
+}
+
+
+for (let i = 0; i < choice.length; i++) {
+    choice[i].addEventListener('click', function (e) {
+
+        if (choice[i].classList.contains('choice-blue')) {
+            if (!choice[i].classList.contains('color-choice-active')) {
+                blue.classList.add('color-choice-active');
+                changeColor("blue");
+            }
+
+            red.classList.remove('color-choice-active');
+            green.classList.remove('color-choice-active');
+            orange.classList.remove('color-choice-active');
+        }
+        else if (choice[i].classList.contains('choice-red')) {
+            if (!choice[i].classList.contains('color-choice-active')) {
+                red.classList.add('color-choice-active');
+                changeColor("red");
+            }
+
+
+            blue.classList.remove('color-choice-active');
+            green.classList.remove('color-choice-active');
+            orange.classList.remove('color-choice-active');
+        }
+        else if (choice[i].classList.contains('choice-green')) {
+            if (!choice[i].classList.contains('color-choice-active')) {
+                green.classList.add('color-choice-active');
+                changeColor("green");
+            }
+
+            blue.classList.remove('color-choice-active');
+            red.classList.remove('color-choice-active');
+            orange.classList.remove('color-choice-active');
+        }
+        else if (choice[i].classList.contains('choice-orange')) {
+            if (!choice[i].classList.contains('color-choice-active')) {
+                orange.classList.add('color-choice-active');
+                changeColor("orange");
+            }
+
+            blue.classList.remove('color-choice-active');
+            green.classList.remove('color-choice-active');
+            red.classList.remove('color-choice-active');
+        }
+    })
+}
+
+
--- src/scripts/load.js
+++ src/scripts/load.js
@@ -26,8 +26,9 @@ async function load() {
         
 
         //DISPLAY ELEMENT
+
         thoughts_list.innerHTML += `
-            <div class = 'item'>
+            <div class = 'item' style="background-image: var(--gradient-${thought.color})" onmouseover="this.style.boxShadow = '0 1px 5px var(--clr-shadow), 0 0 50px var(--clr-shadow-${thought.color})'" onmouseleave="this.style.boxShadow = 'none'">
                 <span>${thought.thought_name}</span>
             </div>
         `
